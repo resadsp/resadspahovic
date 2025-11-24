f99c30898b2d61f736cf8cffb89ab035a3cc5f6d
resadsp <resadspahovic94@gmail.com>
2025-11-24 16:50:02 +0100

    dodavanje zadnjih stavki

--- assets/js/main.js
+++ assets/js/main.js
@@ -298,7 +298,7 @@
         loadBlob().then(url => {
           const a = document.createElement('a');
           a.href = url;
-          a.download = 'My_CV.pdf';
+          a.download = 'Resad_Spahovic_CV.pdf';
           document.body.appendChild(a);
           a.click();
           a.remove();
